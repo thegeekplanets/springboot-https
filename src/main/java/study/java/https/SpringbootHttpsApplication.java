e542e6351fbfb9d252da4027be1f21ccef14f111
Haridas Kanure <hkanure@dminc.com>
2024-03-17 16:53:52 +0530

    first commit

---
+++ src/main/java/study/java/https/SpringbootHttpsApplication.java
@@ -0,0 +1,13 @@
+package study.java.https;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHttpsApplication.class, args);
+	}
+
+}
